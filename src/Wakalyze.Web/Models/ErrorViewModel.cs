01fd2108c59e5a4fae12d7dd130c93c9d2c46dbb
Cagdas Korkut <cagdaskorkut@gmail.com>
2018-07-11 21:47:49 +0300

    changed to mvc... what was I thinking

---
+++ src/Wakalyze.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Wakalyze.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
